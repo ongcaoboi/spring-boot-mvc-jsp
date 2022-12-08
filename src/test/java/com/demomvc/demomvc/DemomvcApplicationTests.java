31158370933f9dd133fe8207fa7f587c9a81c1e4
ongcaoboi <tuananhafiy@gmail.com>
2022-12-08 21:16:38 +0700

    first commit

---
+++ src/test/java/com/demomvc/demomvc/DemomvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demomvc.demomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
